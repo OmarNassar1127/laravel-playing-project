ada5f74d9b2c031e4e86bdfbe01906bbc9e03bed
Omar Nassar <omar.portero.nassar1@gmail.com>
2023-06-30 14:07:10 +0200

    form packages

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,12 +2,27 @@
 
 @section('content')
 <div>
-  <form action="/posts" method="POST">
+  {{-- <form action="/posts" method="POST"> --}}
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\PostController@store' ]) !!}
     @csrf
-    <input type="text" name="title" placeholder="Enter title">
-    <input type="text" name="body" placeholder="Enter body content here">
-    <input type="submit">
-  </form>
+
+    <div class="form-group">
+
+      {!! Form::label('title', 'Title:') !!}
+      {!! Form::text('title', null, ['class' => 'form-controll']) !!}
+      <br>
+      {!! Form::label('body', 'Body:') !!}
+      {!! Form::text('body', null, ['class' => 'form-controll']) !!}
+      <br>
+      {!! Form::submit('Create Post', ['class'=> 'btn btn-primary'])!!}
+      {!! Form::close() !!}
+    </div>
+
+
+    {{-- <input type="text" name="title" placeholder="Enter title">
+    <input type="text" name="body" placeholder="Enter body content here"> --}}
+    {{-- <input type="submit"> --}}
+  {{-- </form> --}}
 </div>
 @endsection
 @section('footer')
